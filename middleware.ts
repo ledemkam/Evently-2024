ff6e2d5e6f038df80dbeac1acf2334b86bf450b3
ledemkam <ledemkam@gmail.com>
2024-03-22 14:05:00 +0100

    first commit

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/webhooks(.*)",
+  ],
+  ignoredRoutes: ["/api/webhook/clerk", "/api/webhook/stripe"],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
+};
