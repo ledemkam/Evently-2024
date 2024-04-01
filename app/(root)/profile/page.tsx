f9438c73bb63c575c0d8b8355d12bbd52a84b619
ledemkam <ledemkam@gmail.com>
2024-04-01 14:01:08 +0200

    order

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,28 +1,42 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
-const Profil = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
+
   return (
     <>
       {/* My Tickets */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="wrapper flex items-center justify-center sm:justify-between">
+        <div className="wrapper flex items-center justify-around sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
-          <Button asChild size="lg" className="button hidden sm:flex">
+          <Button
+            asChild
+            size="lg"
+            className="button rounded-full hidden sm:flex"
+          >
             <Link href="/#events">Explore More Events</Link>
           </Button>
         </div>
       </section>
 
       <section className="wrapper my-8">
-        {/* <Collection 
+        <Collection
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
@@ -31,14 +45,18 @@ const Profil = async () => {
           page={ordersPage}
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
-        /> */}
+        />
       </section>
 
       {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="wrapper flex items-center justify-center sm:justify-between">
+        <div className="wrapper flex items-center justify-around sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
-          <Button asChild size="lg" className="button hidden sm:flex">
+          <Button
+            asChild
+            size="lg"
+            className="button rounded-full hidden sm:flex"
+          >
             <Link href="/events/create">Create New Event</Link>
           </Button>
         </div>
@@ -51,7 +69,7 @@ const Profil = async () => {
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
           limit={3}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
           totalPages={organizedEvents?.totalPages}
         />
@@ -59,4 +77,5 @@ const Profil = async () => {
     </>
   );
 };
-export default Profil;
+
+export default ProfilePage;
